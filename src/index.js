2cbd757872f036c1145161d89902856bc8d3b02f
Heron <heronmaioli@hotmail.com>
2022-01-14 14:39:26 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -160,7 +160,6 @@ io.on("connection", (socket) => {
     console.log(
       "_______________________________________________________________"
     );
-    sockets = fil;
   });
 
   socket.on("disconnect", (reason) => {
